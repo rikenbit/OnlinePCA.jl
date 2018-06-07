9022f657a5f9c5a076987bec3f5553b2225dceac
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2018-06-07 15:03:59 +0900

    @show W

--- src/oja.jl
+++ src/oja.jl
@@ -69,6 +69,7 @@ function oja(input, outdir, scale, pseudocount, rowmeanlist, rowvarlist, colsuml
                 read!(stream, x)
                 normx = normalizex(x, n, stream, scale, pseudocount, masklist, maskvec, rowmeanlist, rowmeanvec, rowvarlist, rowvarvec, colsumlist, colsumvec)
                 # Update Eigen vector
+                @show W
                 W, v = ojaupdate(scheduling, stepsize, g, epsilon, D, N, M, W, v, normx, s, n)
                 # NaN
                 checkNaN(N, s, n, W, pca)
