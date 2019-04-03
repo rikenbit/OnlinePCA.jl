235eff4f1b9da858f4f0fb16c94ac481b17206f9
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2019-04-03 12:26:12 +0900

    debugged

--- src/tenxpca.jl
+++ src/tenxpca.jl
@@ -185,7 +185,7 @@ function tenxpca(tenxfile, outdir, scale, rowmeanlist, rowvarlist, colsumlist, d
         # 遅い
         println("Xmean*Ω")
         for n in startp:endp
-            XmeanΩ[n,:] .= ((zeros(Int, 1, M) .+ rowmeanvec[n])*Ω)[1,:]
+            XmeanΩ[n,:] .= sum(rowmeanvec[n]*Ω, dims=1)
         end
         # for m in 1:M
         #     XmeanΩ[startp:endp,:] .+= rowmeanvec[startp:endp]*Ω[m,:]'
@@ -241,7 +241,7 @@ function tenxpca(tenxfile, outdir, scale, rowmeanlist, rowvarlist, colsumlist, d
             # 遅い
             println("rowmeanvec * AtL[m,:]'")
             for n = startp:endp
-                XmeanAtL[n,:] .= ((zeros(Int, 1, M) .+ rowmeanvec[n])*AtL)[1,:]
+                XmeanAtL[n,:] .= sum(rowmeanvec[n]*AtL, dims=1)
             end
             # for m = 1:M
             #     XmeanAtL .+= rowmeanvec * AtL[m,:]'
